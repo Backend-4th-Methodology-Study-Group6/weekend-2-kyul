1f5da5f035e61ea8d96fc516e672e008b5e60f75
KimH4nKyul <alskim93@kakao.com>
2024-03-13 19:26:30 +0900

    :goal_net:

--- 3주차/board-service-with-tdd/test/user/small/user.domain.test.ts
+++ 3주차/board-service-with-tdd/test/user/small/user.domain.test.ts
@@ -1,9 +1,20 @@
 export class User {
   static create(id: string, password: string): User {
-    if (id.length < 8) throw Error(`아이디는 8자 이상이어야 합니다.`);
+    if (id.length < 8)
+      throw new UserIdValidationError(`아이디는 8자 이상이어야 합니다.`);
     if (/[^\w]/gi.exec(id))
-      throw Error(`아이디는 특수문자를 사용할 수 없습니다.`);
+      throw new UserIdValidationError(
+        `아이디는 특수문자를 사용할 수 없습니다.`,
+      );
     return new User();
+  }
+}
+
+export class UserIdValidationError extends Error {
+  readonly statusCode: number = 400;
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, UserIdValidationError.prototype);
   }
 }
 
@@ -16,7 +27,7 @@ describe(`유저 도메인`, () => {
     // then
     expect(() => {
       User.create(id, password);
-    }).toThrow(new Error(`아이디는 8자 이상이어야 합니다.`));
+    }).toThrow(new UserIdValidationError(`아이디는 8자 이상이어야 합니다.`));
   });
   it(`❌ 유저를 생성할 수 없음 - 특수문자를 사용한 아이디`, () => {
     // given
@@ -26,7 +37,9 @@ describe(`유저 도메인`, () => {
     // then
     expect(() => {
       User.create(id, password);
-    }).toThrow(new Error(`아이디는 특수문자를 사용할 수 없습니다.`));
+    }).toThrow(
+      new UserIdValidationError(`아이디는 특수문자를 사용할 수 없습니다.`),
+    );
   });
   it(`❌ 유저를 생성할 수 없음 - 길이가 짧은 패스워드`, () => {});
   it(`❌ 유저를 생성할 수 없음 - 특수문자가 포함되지 않은 패스워드`, () => {});
