8380396a977a827e033f0e9eedb53117784a5614
KimH4nKyul <alskim93@kakao.com>
2024-03-14 21:29:50 +0900

    :memo: README

--- 3주차/board-service-with-tdd/src/app.module.ts
+++ 3주차/board-service-with-tdd/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { BoardModule } from './board/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule, BoardModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule],
 })
 export class AppModule {}
